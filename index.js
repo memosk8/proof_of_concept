88deda3e2f0209b32b2828177010318eb9e29ff9
memosk8 <memolopez9363@gmail.com>
2023-03-10 14:43:28 -0600

    getting metadata from google sheets

--- index.js
+++ index.js
@@ -4,8 +4,26 @@ const
   app = express(),
   port = 1337;
 
-app.get('/', (req, res) => {
-  res.send("Hello there kenobi...")
+app.get('/', async (req, res) => {
+
+  const auth = new google.auth.GoogleAuth({
+    keyFile: "credentials.json",
+    scopes: "https://www.googleapis.com/auth/spreadsheets",
+  });
+
+  // create client instance for auth
+  const 
+    spreadsheetId = "1ZxBAdpCSB63mcMkw4JFFDumfXPpX-U-Iagm4kR2d5w8",
+    client = await auth.getClient(),
+    // Instance of google sheets api
+    googleSheets = google.sheets({version: "v4", auth: client}),
+    // get metadata from spreadsheet
+    metaData = await googleSheets.spreadsheets.get({
+      auth, spreadsheetId
+    });
+
+
+    res.send(metaData)
 })
 
-app.listen(port, (req,res) => console.log(`running on port ${port}`))
+app.listen(port, (req,res) => console.log(`running on port ${port}`));
