0e9fca9f1de508bd192303c2844aaff3bdc96200
/home/memosk8 <memolopez9363@gmail.com>
2023-03-09 19:28:16 -0600

    add dev script and port number

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const 
+const
   express = require('express'),
-  {google} = require('googleapis'),
-  app = express();
+  { google } = require('googleapis'),
+  app = express(),
+  port = 1337;
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
   res.send("Hello there kenobi...")
-} )
+})
+
+app.listen(port, (req,res) => console.log(`running on port ${port}`))
